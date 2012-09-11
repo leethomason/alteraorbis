fec91d849a74a3529be3fc615caea1fb9c82ff78
Lee Thomason (grinliz) <leethomason@gmail.com>
2012-09-10 21:51:13 -0700

    missing file

---
+++ xegame/itemcomponent.cpp
@@ -0,0 +1,27 @@
+#include "itemcomponent.h"
+#include "chit.h"
+#include "../game/healthcomponent.h"
+
+void ItemComponent::OnChitMsg( Chit* chit, const ChitMsg& msg )
+{
+	if ( msg.ID() == MSG_CHIT_DAMAGE ) {
+		const DamageDesc* dd = (const DamageDesc*) msg.Ptr();
+		GLASSERT( dd );
+		GLLOG(( "Chit %3d '%s' ", parentChit->ID(), item.Name() ));
+
+		// FIXME: see if inventory can absorb first (shields)
+
+		float hp = item.hp;
+		item.AbsorbDamage( *dd, 0, "DAMAGE" );
+		GLLOG(( "\n" ));
+
+		if ( item.hp != hp ) {
+			HealthComponent* hc = GET_COMPONENT( parentChit, HealthComponent );
+			if ( hc ) {
+				hc->DeltaHealth();
+			}
+		}
+	}
+}
+
+
