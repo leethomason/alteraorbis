52395d07486af911b2ac44bdab7b65463658f48f
Lee Thomason (grinliz) <leethomason@gmail.com>
2012-10-07 16:51:31 -0700

    correct values seem to get generated for database. metadata still off

--- builder/animationbuilder.cpp
+++ builder/animationbuilder.cpp
@@ -38,6 +38,9 @@ void SCMLParser::ReadPartNames( const tinyxml2::XMLDocument* doc )
 		const char* name = strstr( fullName, "/" );
 		GLASSERT( name );
 		name++;
+		char* end = (char*)strrchr( name, '.' );
+		GLASSERT( end );
+		*end = 0;
 
 		GLString str( name );
 		//GLOUTPUT(( "adding [%d]=%s\n", id, str.c_str() ));
@@ -56,6 +59,7 @@ void SCMLParser::ReadAnimations( const tinyxml2::XMLDocument* doc )
 	{
 		Animation a;
 		a.name = animationEle->Attribute( "name" );
+
 		animationEle->QueryUnsignedAttribute( "length", &a.length );
 		animationEle->QueryIntAttribute( "id", &a.id );
 		//GLOUTPUT(( "Animation: %s len=%d id=%d\n", a.name.c_str(), a.length, a.id ));
@@ -155,6 +159,7 @@ void SCMLParser::ReadAnimation( const XMLDocument* doc, Animation* a )
 			objectEle->QueryFloatAttribute( "y", &y );
 			objectEle->QueryFloatAttribute( "angle", &angle );
 
+			a->frames[keyCount].xforms[partID].id    = partID;
 			a->frames[keyCount].xforms[partID].angle = angle;
 			a->frames[keyCount].xforms[partID].x	 = x;
 			a->frames[keyCount].xforms[partID].y	 = y;
@@ -175,10 +180,10 @@ void SCMLParser::WriteAnimation( gamedb::WItem* witem, const Animation& a, const
 
 		for( int k=0; k<EL_MAX_BONES; ++k ) {
 			GLString name;
-			GLASSERT( k == f.xforms[k].id );	
 
-			if ( partIDNameMap.Query( f.xforms[k].id, &name ) ) {
-				const PartXForm& xform = f.time[k];
+			if ( partIDNameMap.Query( k, &name ) ) {
+				GLASSERT( k == f.xforms[k].id );	
+				const PartXForm& xform = f.xforms[k];
 				gamedb::WItem* bone = frameItem->CreateChild( name.c_str() );
 
 				float anglePrime = 0;
@@ -199,8 +204,8 @@ void SCMLParser::WriteAnimation( gamedb::WItem* witem, const Animation& a, const
 				float rx=0, ry=0;
 
 				if ( reference ) {
-					rx = reference->frames[k].xforms[k].x / pur;
-					ry = reference->frames[k].xforms[k].y / pur;
+					rx = reference->frames[0].xforms[k].x / pur;
+					ry = reference->frames[0].xforms[k].y / pur;
 
 					// Part to origin. Rotate. Back to pos. Apply delta.
 					Matrix4 toOrigin, toPos, rot;
@@ -222,7 +227,7 @@ void SCMLParser::WriteAnimation( gamedb::WItem* witem, const Animation& a, const
 }
 
 
-void SCMLParser::Parse( const tinyxml2::XMLDocument* doc, gamedb::WItem* witem )
+void SCMLParser::Parse( const tinyxml2::XMLDocument* doc, gamedb::WItem* witem, float pur )
 {
 	ReadPartNames( doc );
 	ReadAnimations( doc );
@@ -260,124 +265,28 @@ void SCMLParser::Parse( const tinyxml2::XMLDocument* doc, gamedb::WItem* witem )
 		}
 	}
 	for( int i=0; i<animationArr.Size(); ++i ) {
-		WriteAnimation( witem, animationArr[i], reference == &animationArr[i] ? 0 : reference );
+		WriteAnimation( witem, animationArr[i], reference == &animationArr[i] ? 0 : reference, pur );
 	}
-}
 
+/*
+	// -- Process the meta data -- //
+	for(	const XMLElement* metaEle = element->FirstChildElement( "meta" );
+			metaEle;
+			metaEle = metaEle->NextSiblingElement( "meta" ) )
+	{
+		const char* animationName = metaEle->Attribute( "animation" );
+		const char* metaName = metaEle->Attribute( "name" );
+		float time = 0;
+		metaEle->QueryFloatAttribute( "time", &time );
 
-
-GLString GetBoneName( const XMLElement* spriteEle )
-{
-	GLString boneName = spriteEle->FirstChildElement( "image" )->GetText();
-	boneName = boneName.substr( 7, 1000 );					// remove "asset/"
-	boneName = boneName.substr( 0, boneName.size()-4 );		// remove ".png"
-	return boneName;
-}
-
-
-const XMLElement* InsertFrame(	gamedb::WItem* frame, 
-								const tinyxml2::XMLElement* reference, 
-								const tinyxml2::XMLElement* frameEle, 
-								const char* frameName,
-								float pur )
-{
-	// Find the frame, in this case the frame description,
-	// not the character frame.
-	
-	for ( ; frameEle; frameEle=frameEle->NextSiblingElement( "frame" ) ) {
-		const char* name = frameEle->FirstChildElement( "name" )->GetText();
-		if ( StrEqual( name, frameName ) ) {
-
-			int i=0;
-			for( const XMLElement* spriteEle = frameEle->FirstChildElement( "sprite" );
-				 spriteEle;
-				 spriteEle = spriteEle->NextSiblingElement( "sprite" ), ++i )
-			{
-				GLString boneName = GetBoneName( spriteEle );
-				gamedb::WItem* bone = frame->CreateChild( boneName.c_str() );
-				
-				float x=0, y=0, angle=0, anglePrime=0;
-
-				spriteEle->FirstChildElement( "angle" )->QueryFloatText( &angle );
-				spriteEle->FirstChildElement( "x"     )->QueryFloatText( &x );
-				spriteEle->FirstChildElement( "y"     )->QueryFloatText( &y );
-
-				angle = NormalizeAngleDegrees( angle );
-				x /= pur;
-				y /= pur;
-
-				// Spriter transformation is written as the position of the bitmap, relative
-				// to the model origin, followed by a rotation. (Origin of the bitmap is 
-				// the upper left in this version.)
-				//
-				// Lumos needs the transformation (delta) from the origin.
-				// To translate, we need the reference (untransformed) frame.
-				Matrix4 m;
-				float rx=0, ry=0;
-
-				if ( reference ) {
-					const XMLElement* refSpriteEle = 0;
-					for( refSpriteEle = reference->FirstChildElement( "sprite" );
-						 refSpriteEle;
-						 refSpriteEle = refSpriteEle->NextSiblingElement( "sprite" ))
-					{
-						GLString refBoneName = GetBoneName( refSpriteEle );
-						if ( boneName == refBoneName ) {
-							break;
-						}
-					}
-					GLASSERT( refSpriteEle );
-
-					refSpriteEle->FirstChildElement( "x" )->QueryFloatText( &rx );
-					refSpriteEle->FirstChildElement( "y" )->QueryFloatText( &ry );
-					rx /= pur;
-					ry /= pur;
-
-					// Part to origin. Rotate. Back to pos. Apply delta.
-					Matrix4 toOrigin, toPos, rot;
-
-					toOrigin.SetTranslation( 0, ry, -rx );			// negative direction, remembering: 1) y is flipped, 2) x maps to z
-					rot.SetXRotation( -angle );						// rotate, convert to YZ right hand rule
-					toPos.SetTranslation( 0, -y, x );				// positive direction
-
-					m = toPos * rot * toOrigin;
-					//m.Dump( "Bone" );
-					anglePrime = m.CalcRotationAroundAxis( 0 );		// not very meaningful, just used to construct a transformation matrix in the shader
-					//GLOUTPUT(( "anglePrime=%f\n", anglePrime ));
-
-#ifdef DEBUG
-					// Check that the matrix can be reconstructed.
-					Matrix4 shader;
-					float sinTheta = sinf( ToRadian( anglePrime ));
-					float cosTheta = cosf( ToRadian( anglePrime ));
-					shader.m22 = cosTheta;
-					shader.m32 = sinTheta;
-					shader.m23 = -sinTheta;
-					shader.m33 = cosTheta;
-					shader.m24 = m.m24;	// see below
-					shader.m34 = m.m34;	// see below
-
-					GLASSERT( Equal( shader, m ));
-#endif
-				}
-
-				bone->SetFloat( "anglePrime", anglePrime );
-				bone->SetFloat( "dy", m.m24 );
-				bone->SetFloat( "dz", m.m34 );
-
-				/*
-				bone->SetFloat( "angle", NormalizeAngleDegrees( -angle ));
-				bone->SetFloat( "z", x );
-				bone->SetFloat( "y", -y );
-				bone->SetFloat( "rz", rx );
-				bone->SetFloat( "ry", -ry );
-				*/
-			}
-			return frameEle;
-		}
+		gamedb::WItem* animationItem = root->FetchChild( animationName );
+		gamedb::WItem* metaDataItem  = animationItem->FetchChild( "metaData" );
+		gamedb::WItem* eventItem     = metaDataItem->FetchChild( metaName );
+		eventItem->SetFloat( "time", time );
 	}
-	return 0;
+*/
 }
+
 
 
 /*
@@ -404,75 +313,13 @@ void ProcessAnimation( const tinyxml2::XMLElement* element, gamedb::WItem* witem
 	if ( doc.Error() ) {
 		ExitError( "Animation", pathName.c_str(), assetName.c_str(), "tinyxml-2 could not parse scml" );
 	}
+	float pur = 1;
+	element->QueryFloatAttribute( "pur", &pur );
 
 	gamedb::WItem* root = witem->CreateChild( assetName.c_str() );	// "humanFemaleAnimation" 
 
 	SCMLParser parser;
-	parser.Parse( &doc, root );
-
-	const XMLConstHandle docH( doc );
-	const XMLElement* reference = 0;
-	float pur = 1;
-	element->QueryFloatAttribute( "pur", &pur );
-
-	// First pass is to get the reference animation; the 2nd pass parses the various flavors.
-	for( int pass=0; pass<2; ++pass ) {
-
-		const XMLElement* animEle = docH.FirstChildElement( "spriterdata" ).FirstChildElement( "char" ).FirstChildElement( "anim" ).ToElement();
-		for( ; animEle; animEle=animEle->NextSiblingElement( "anim" ) ) {
-
-			const char* animName = animEle->FirstChildElement( "name" )->GetText();
-			if (    ( pass == 0 && StrEqual( animName, "reference" ) )
-				 || ( pass == 1 && !StrEqual( animName, "reference" )))
-			{
-				gamedb::WItem* anim = root->CreateChild( animName );	// "walk"
-				anim->FetchChild( "metaData" );		// always write so that NumFrames = NumChildren - 1
-				int frameCount = 0;
-				float totalDuration = 0;
-
-				for( const XMLElement* frameEle = animEle->FirstChildElement( "frame" );
-					 frameEle;
-					 frameEle = frameEle->NextSiblingElement( "frame" ) )
-				{
-					gamedb::WItem* frame = anim->CreateChild( frameCount++ );
-					float duration = 100.0f;
-					XMLUtil::ToFloat( frameEle->FirstChildElement( "duration" )->GetText(), &duration );
-
-					duration *= 10.0f;	// Totally mystery. Seems to be in 1/100th seconds. Likely Spriter bug.
-					frame->SetFloat( "duration", duration );
-					totalDuration += duration;
-
-					const char* frameName = frameEle->FirstChildElement( "name" )->GetText();
-
-					const XMLElement* f = InsertFrame(	frame, 
-														reference,
-														docH.FirstChildElement( "spriterdata" ).FirstChildElement( "frame" ).ToElement(), 
-														frameName, 
-														pur );
-					if ( pass == 0 ) {
-						reference = f;
-					}
-				}
-				anim->SetFloat( "totalDuration", totalDuration );
-			}
-		}
-	}
-
-	// -- Process the meta data -- //
-	for(	const XMLElement* metaEle = element->FirstChildElement( "meta" );
-			metaEle;
-			metaEle = metaEle->NextSiblingElement( "meta" ) )
-	{
-		const char* animationName = metaEle->Attribute( "animation" );
-		const char* metaName = metaEle->Attribute( "name" );
-		float time = 0;
-		metaEle->QueryFloatAttribute( "time", &time );
-
-		gamedb::WItem* animationItem = root->FetchChild( animationName );
-		gamedb::WItem* metaDataItem  = animationItem->FetchChild( "metaData" );
-		gamedb::WItem* eventItem     = metaDataItem->FetchChild( metaName );
-		eventItem->SetFloat( "time", time );
-	}
+	parser.Parse( &doc, root, pur );
 
 //	fclose( fp );
 }
