034994eb5a0c6e56615a1924ee5fd1733d0b6883
Lee Thomason <leethomason@gmail.com>
2014-02-05 09:14:28 -0800

    basic TowComponent code in place

--- game/towcomponent.cpp
+++ game/towcomponent.cpp
@@ -1,4 +1,14 @@
 #include "towcomponent.h"
+#include "../game/lumoschitbag.h"
+#include "../xegame/chit.h"
+#include "../xegame/rendercomponent.h"
+#include "../xegame/spatialcomponent.h"
+
+using namespace grinliz;
+
+static const float TOW_SPEED		= DEFAULT_MOVE_SPEED * 3.0f;
+static const float TOW_ROTATION		= ROTATION_SPEED * 2.0f;
+static const float TOW_DISTANCE		= 0.5f;
 
 void TowComponent::OnAdd( Chit* chit )
 {
@@ -28,6 +38,56 @@ void TowComponent::DebugStr( grinliz::GLString* str )
 
 int TowComponent::DoTick( U32 delta )
 {
-	return VERY_LONG_TICK;
+	if ( towingID == 0 ) {
+		return VERY_LONG_TICK;
+	}
+
+	ComponentSet thisComp( parentChit, Chit::SPATIAL_BIT | Chit::RENDER_BIT | ComponentSet::IS_ALIVE );
+	if ( !thisComp.okay ) {
+		return VERY_LONG_TICK;
+	}
+
+	const ChitContext* context = this->GetChitContext();
+	LumosChitBag* chitBag = this->GetLumosChitBag();
+	Chit* tow = chitBag->GetChit( towingID );
+	if ( !tow ) {
+		towingID = 0;
+		return VERY_LONG_TICK;
+	}
+
+	ComponentSet towComp( tow, Chit::SPATIAL_BIT | ComponentSet::IS_ALIVE );
+	if ( !towComp.okay ) {
+		return 0;	// try again next frame?
+	}
+
+	if ( delta > MAX_FRAME_TIME ) delta = MAX_FRAME_TIME;
+
+	Vector3F myTarget = thisComp.spatial->GetPosition();
+	thisComp.render->CalcTarget( &myTarget );
+	Vector3F myHeading = thisComp.spatial->GetHeading();
+	Quaternion myRot = thisComp.spatial->GetRotation();
+
+	Vector3F towTarget = myTarget - myHeading * TOW_DISTANCE;
+	Vector3F towPos = towComp.spatial->GetPosition();
+	Vector3F towDir = towTarget - towPos;
+	towDir.Normalize();
+
+	Vector3F v = towTarget;
+	float travel = Travel( TOW_SPEED, delta );
+	if ( (towPos - towTarget).Length() <= travel ) {
+		// All good; will reach target.
+		//towComp.spatial->SetPosition( towTarget );
+	}
+	else {
+		v = towPos + towDir * travel;
+	}
+
+	// Not sure how to reasonably do constant rotation...see how this does:
+	Quaternion towRot = towComp.spatial->GetRotation();
+	Quaternion rot;
+	Quaternion::SLERP( towRot, myRot, 0.2f, &rot );
+	towComp.spatial->SetPosRot( v, rot );
+
+	return 0;
 }
 
