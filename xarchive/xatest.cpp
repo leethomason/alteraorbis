b79ad713314bb42111aa715b4bc03ba30631f259
Lee Thomason <leethomason@gmail.com>
2014-03-23 11:24:03 -0700

    tweak the float compression

--- xarchive/xatest.cpp
+++ xarchive/xatest.cpp
@@ -116,6 +116,49 @@ void KeyAttributes( XStream* xs )
 }
 
 
+void FloatEncoding(XStream* xs) 
+{
+	float oneF = 1;
+	float zeroF = 0;
+	float point5F = 1023.0f + 0.5f;
+	float intF = 1023;
+	float valueF = 1.23456f;
+
+	double oneD = 1;
+	double zeroD = 0;
+	double point5D = 1023.0 + 0.5;
+	double intD = 1023;
+	double valueD = 1.23456;
+
+	XarcOpen(xs, "FloatEncoding");
+	XARC_SER(xs, oneF);
+	XARC_SER(xs, zeroF);
+	XARC_SER(xs, point5F);
+	XARC_SER(xs, intF);
+	XARC_SER(xs, valueF);
+
+	XARC_SER(xs, oneD);
+	XARC_SER(xs, zeroD);
+	XARC_SER(xs, point5D);
+	XARC_SER(xs, intD);
+	XARC_SER(xs, valueD);
+	XarcClose(xs);
+
+	GLASSERT(oneF == 1.0f);
+	GLASSERT(zeroF == 0.0f);
+	GLASSERT(point5F = 1023.5f);
+	GLASSERT(intF == 1023);
+	GLASSERT(valueF == 1.23456f);
+
+	GLASSERT(oneD == 1.0);
+	GLASSERT(zeroD == 0.0);
+	GLASSERT(point5D = 1023.5);
+	GLASSERT(intD == 1023);
+	GLASSERT(valueD == 1.23456);
+}
+
+
+
 int main( int argc, const char* argv[] ) 
 {
 	printf( "Xarchive test.\n" );
@@ -132,6 +175,7 @@ int main( int argc, const char* argv[] )
 			Map( &writer );
 			MetaData( &writer );
 			Limits( &writer );
+			FloatEncoding(&writer);
 			writer.CloseElement();
 
 			fclose( fp );
@@ -145,6 +189,7 @@ int main( int argc, const char* argv[] )
 			Map( &reader );
 			MetaData( &reader );
 			Limits( &reader );
+			FloatEncoding(&reader);
 			reader.CloseElement();
 	
 			fclose( fp );
