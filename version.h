3f1816063070c2316651a03a5864508e0eb0886d
Lee Thomason <leethomason@gmail.com>
2015-04-10 09:31:51 -0700

    up version, adjust costs

--- version.h
+++ version.h
@@ -1,8 +1,9 @@
-#define VERSION "Beta 3i" 
+#define VERSION "Beta 4a" 
 
 //static const int CURRENT_FILE_VERSION = 6;	Beta1
 //static const int CURRENT_FILE_VERSION = 7;	Beta2
 //static const int CURRENT_FILE_VERSION = 8;		// Beta3
 //static const int CURRENT_FILE_VERSION = 9;		// Beta3b
-static const int CURRENT_FILE_VERSION = 10;		// Beta3c
+//static const int CURRENT_FILE_VERSION = 10;		// Beta3c
+static const int CURRENT_FILE_VERSION = 11;		// Beta3c
 
