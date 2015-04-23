fc6c3840d5c941a113bae3689750d4a686350430
Lee Thomason <leethomason@gmail.com>
2015-04-22 17:22:26 -0700

    version 4a

--- version.h
+++ version.h
@@ -5,5 +5,6 @@
 //static const int CURRENT_FILE_VERSION = 8;		// Beta3
 //static const int CURRENT_FILE_VERSION = 9;		// Beta3b
 //static const int CURRENT_FILE_VERSION = 10;		// Beta3c
-static const int CURRENT_FILE_VERSION = 11;		// Beta3c
+//static const int CURRENT_FILE_VERSION = 11;		// Beta3c
+static const int CURRENT_FILE_VERSION = 12;		// Beta4a
 
