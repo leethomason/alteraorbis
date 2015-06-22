1a0b09cd826d9198598cf1de8dd6264d45ee296d
Lee Thomason <leethomason@gmail.com>
2015-06-22 07:32:36 -0700

    linux fixes and build file

---
+++ buildlinux.sh
@@ -0,0 +1,26 @@
+# Initial build
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make
+cd ..
+
+# Resources
+build/builder resin/default.xml res/lumos.db
+
+# Set up everything
+rm -rf AlteraOrbis
+
+mkdir AlteraOrbis
+mkdir AlteraOrbis/res
+
+cp res/* AlteraOrbis/res
+cp build/alteraorbis AlteraOrbis
+cp README.txt AlteraOrbis
+
+tar czf AlteraOrbisLinux_$1.tar.gz AlteraOrbis
+
+# tar -zxvf AlteraOrbisLinux.tar.gz
+
+
+
